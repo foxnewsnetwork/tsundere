0d8e20684a5ff794ab554df07f29caa7a98c649e
Thomas Chen <foxnewsnetwork@gmail.com>
2012-12-13 08:35:26 -0800

    put in another file for rails extensions

--- lib/tsundere.rb
+++ lib/tsundere.rb
@@ -5,14 +5,19 @@ require_relative "./tsundere/array"
 require_relative "./tsundere/class_methods"
 require_relative "./tsundere/instance_methods"
 require_relative "./tsundere/imouto"
+require_relative "./tsundere/rails_extension"
 module Tsundere
 	def self.included(base)
 		base.extend ClassMethods
+		base.extend RailsExtension
 	end # self.included
 end # Tsundere
 
 if defined? Rails and defined? ActiveSupport
+	# ActiveSupport.on_load(:action_view) do
+		
+	# end # onload
 	ActiveSupport.on_load(:action_controller) do
 		include Tsundere
-	end # onload
+	end # 
 end # if
